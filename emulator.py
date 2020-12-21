8f5f15b8ffc1945f43976dd412cb7a2b0a182ea9
Raymond Moreau <rmore082@uottawa.ca>
2020-12-21 18:54:21 -0500

    Created sample interface. Some bugs left to fix.

--- emulator.py
+++ emulator.py
@@ -2,6 +2,8 @@
 # Raymond and Ashton's Pokemon Emulator
 #####################
 
+import random
+
 class Pokemon:
     """Pokemon class"""
 
@@ -37,16 +39,16 @@ class Pokemon:
         Type (str)
         Level (int)
         """
-        name = input("What is the pokemons name?").strip().lower().capitalize()
+        name = input("What is the pokemons name? ").strip().lower().capitalize()
 
-        t = input("What is " + name + "'s type?").strip().lower().capitalize()
+        t = input("What is " + name + "'s type? ").strip().lower().capitalize()
 
         check = True
         while check:
             # this system ensures pokemon level is a valid number (1-100)
             # will loop through until valid number is entered
 
-            level = input("What is " + name + "'s level?").strip()
+            level = input("What is " + name + "'s level? ").strip()
 
             try:  # tests for if input was an integer, if so, executes code.
                 level = int(level)
@@ -68,7 +70,7 @@ class Pokemon:
             # this system ensures pokemon health is a valid number (0-infinity)
             # will loop through until valid number is entered
 
-            health = input("What is " + name + "'s health?").strip()
+            health = input("What is " + name + "'s health? ").strip()
 
             try:  # tests for if input was an integer, if so, executes code.
                 health = int(health)
@@ -111,27 +113,110 @@ class Pokemon:
 ###########################################################################################
 ###########################################################################################
 
-shakila = Pokemon()
+input("Welcome to the world of Pokemon! Let's get started by creating your pokemon. Press Enter to continue.\n")
 
-shakila.createpokemon()
+poke = Pokemon()
 
-print(shakila)
+poke.createpokemon()
 
-while shakila.hp > 0:
-    damage = int(input("How hard do you want to hit Shakila?"))
+print("\nCongratulations! You have created your own Pokemon! Here's what it looks like:")
 
-    shakila.takedamage(damage)
+print(poke)
 
-    print(shakila)
+input("Press enter to continue.\n")
 
-print("Congrats! You knocked out Shakila!")
+print("We will now fight a training dummy. Use your moves to take it down before it beats you!")
 
-while shakila.hp < 1:
+dummy = Pokemon("Dummy", "Dummy", 5, 20)
 
-    healing = int(input("How hard do you want to heal Shakila?"))
+print("Here's the dummies stats: " + str(dummy))
 
-    shakila.gainhealth(healing)
+input("Press Enter to continue into the fight!\n")
 
-    print(shakila)
+charge = 0
 
-print("Boo. You saved Shakila.")
+while dummy.hp > 0: # battles until dummy is 'dead'
+
+    print("Here's how much health the dummy has left: " + str(dummy.hp))
+
+    charge = charge + 1
+
+    print("Let's attack the dummy! Here are your moves:")
+
+    print("(1) Light attack: 5 damage")
+
+    print("(2) Strong attack: 10 damage (" + str(charge) + "/3 charges, generates one charge per turn)\n")
+
+    answer = int(input("Type 1 or 2 to choose which attack to use! (Can't choose strong attack unless charged)\n").strip())
+
+    check = True
+
+    while check: # ensures input was a valid attack
+        if answer != type(int):
+
+            print("That is not an number. Type in 1 or 2 to use a move!")
+
+            answer = input("Type 1 or 2 to choose which attack to use! (Can't choose strong attack unless charged)\n").strip()
+
+        elif answer > 2:
+
+            print("That is not 1 or 2. Type in 1 or 2 to use a move!")
+
+            answer = input("Type 1 or 2 to choose which attack to use! (Can't choose strong attack unless charged)\n").strip()
+
+        elif answer < 1:
+
+            print("That is not 1 or 2. Type in 1 or 2 to use a move!")
+
+            answer = input("Type 1 or 2 to choose which attack to use! (Can't choose strong attack unless charged)\n").strip()
+
+        elif answer == 2 and charge < 3:
+
+            print("You cannot use the Strong attack (2) because it is not charged!")
+
+            answer = input("Type 1 or 2 to choose which attack to use! (Can't choose strong attack unless charged)\n").strip()
+
+        else:
+            check = False
+
+    # pokemon doing damage phase
+
+    if answer == 1:
+
+        print("You chose the light attack! Get him, " + poke.name + "!")
+
+        print(poke.name + " did 5 damage to the dummy!")
+
+        dummy.takedamage(5)
+
+    if answer == 2:
+
+        print("You chose the heavy attack! Hit him hard, " + poke.name + "!")
+
+        print(poke.name + " did 10 damage to the dummy!")
+
+        dummy.takedamage(10)
+
+    # dummy doing damage phase
+
+    if dummy.hp > 0:
+
+        input("Good job, " + poke.name + "! Uh-oh, it looks like the dummy is fighting back! Press Enter to continue.\n")
+
+        dummydam = random.randint(1, 10)
+
+        print("The dummy hit " + poke.name + "! It did " + str(dummydam) + " damage!")
+
+        poke.takedamage(dummydam)
+
+    if poke.hp < 0:
+
+        print("Oh no! " + str(poke.hp) + " fainted! We lost the battle!")
+
+        dummy.hp = 0
+
+        print("Better luck next time!")
+
+    if dummy.hp < 0:
+
+        print("you did it good job die")
